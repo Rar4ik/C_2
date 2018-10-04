ee134df05c16718c20b512d8b14e7f4cd627745f
Rar <mravaev@yandex.ru>
2018-10-04 16:45:27 +0300

    C#2_1

---
+++ C2_1/C2_1/BaseObj.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+
+
+namespace C2_1
+{
+    class BaseObj
+    {
+        protected Point Pos;
+        protected Point Dir;
+        protected Size Size;
+        public BaseObj(Point pos, Point dir, Size size)
+        {
+            Pos = pos;
+            Dir = dir;
+            Size = size;
+        }
+        public virtual void Draw()
+        {
+            Game.Buffer.Graphics.DrawEllipse(Pens.White, Pos.X, Pos.Y, Size.Width, Size.Height);
+        }
+        public virtual void Update()
+        {
+            Pos.X = Pos.X + Dir.X;
+            Pos.Y = Pos.Y + Dir.Y;
+            if (Pos.X < 0) Dir.X = -Dir.X;
+            if (Pos.X > Game.Widht) Dir.X = -Dir.X;
+            if (Pos.Y < 0) Dir.Y = -Dir.Y;
+            if (Pos.Y > Game.Hight) Dir.Y = -Dir.Y;
+        }        
+        //public void CreateImage()
+        //{
+        //    Image newimage = Image.FromFile(@"C:\Users\Михаил\Pictures\Разработка\meteor.jpg");
+        //    Rectangle rect = new Rectangle(Pos.X, Pos.Y, Size.Width, Size.Height);
+        //    Game.Buffer.Graphics.DrawImage(newimage, rect);
+
+        //}
+        
+    }
+}
---
+++ C2_1/C2_1/Star.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace C2_1
+{
+    class Star: BaseObj
+    {
+        public Star(Point pos, Point dir, Size size): base(pos,dir,size)
+        {
+        }
+        public override void Draw()
+        {
+            Game.Buffer.Graphics.DrawLine(Pens.DarkRed, Pos.X, Pos.Y, Pos.X + Size.Width, Pos.Y + Size.Height);
+            Game.Buffer.Graphics.DrawLine(Pens.DarkRed, Pos.X + Size.Width, Pos.Y, Pos.X, Pos.Y + Size.Height);
+        }
+        public override void Update()
+        {
+            Pos.X = Pos.X - Dir.X;
+            //Pos.Y = Pos.Y - Dir.Y;
+            if (Pos.X < 0) Pos.X = Game.Widht + Size.Width;
+            //if (Pos.Y < 0) Pos.Y = Game.Hight + Size.Height;
+        }
+    }
+}
