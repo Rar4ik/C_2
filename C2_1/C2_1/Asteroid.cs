45d9f5c6823089928ffcbe70ac74aeace80d1b72
Rar <mravaev@yandex.ru>
2018-10-11 16:24:55 +0300

    lesson 3

--- C2_1/C2_1/Asteroid.cs
+++ C2_1/C2_1/Asteroid.cs
@@ -8,18 +8,35 @@ using System.Drawing;
 
 namespace C2_1
 {
-    class Asteroid: BaseObj
+    class Asteroid : BaseObj, ICloneable, IComparable<Asteroid>
     {
-        public int Power { get; set; }
+        public interface IComparable<T>
+        {
+            int CompareTo(T obj);
+        }
+        public int CompareTo(Asteroid obj)
+        {
+            if (Power > obj.Power)
+                return 1;
+            if (Power < obj.Power)
+                return -1;
+            return 0;
+        }
+        public int Power { get; set; } = 3;
         public Asteroid(Point pos, Point dir, Size size) : base(pos, dir, size)
         {
             Power = 1;
         }
-
+        public object Clone()
+        {
+            Asteroid asteroid = new Asteroid(new Point(Pos.X, Pos.Y), new Point(Dir.X, Dir.Y), new Size(Size.Width, Size.Height)) { Power = Power };
+            return asteroid;
+        }
+        
         public override void Draw()
         {
             Game.Buffer.Graphics.DrawEllipse(Pens.White, Pos.X, Pos.Y, Size.Width, Size.Height);
-        }
+        }     
         /// <summary>
         /// отражение астероидов от стен
         /// </summary>
@@ -33,7 +50,6 @@ namespace C2_1
             if (Pos.Y > Game.Hight) Dir.Y = -Dir.Y;
             //Pos.X = Pos.X - Dir.X;
             //if (Pos.X < 0) Pos.X = Game.Widht + Size.Width;
-
         }
         /// <summary>
         /// Добавлена возможность регенирировать метеоры в разных участках карты
